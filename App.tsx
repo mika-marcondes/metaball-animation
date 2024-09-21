51e8c2ce6ec869a5dde9100c611a99b7ba63c5b3
Mikael <mika.marcondes2@gmail.com>
2024-09-21 13:08:41 -0300

    NOW move circle around

--- App.tsx
+++ App.tsx
@@ -2,6 +2,8 @@ import {useWindowDimensions} from "react-native";
 import Touchable, {useGestureHandler,} from "react-native-skia-gesture"
 import {useSharedValue} from "react-native-reanimated";
 import {GestureHandlerRootView} from "react-native-gesture-handler";
+import {Circle, Group, SweepGradient, vec} from "@shopify/react-native-skia";
+
 
 const radius = 80;
 
@@ -14,23 +16,29 @@ export default function App() {
 
     const context = useSharedValue({x: 0, y: 0});
 
-    const gestureHandler = useGestureHandler({
+    const circleGesture = useGestureHandler({
         onStart: () => {
+            'worklet';
             context.value = {
                 x: cx.value,
                 y: cy.value,
             }
         },
         onActive: ({translationX, translationY}) => {
+            'worklet';
             cx.value = context.value.x + translationX;
             cy.value = context.value.y + translationY;
-        }
+        },
     })
 
     return (
         <GestureHandlerRootView>
             <Touchable.Canvas style={{flex: 1, backgroundColor: 'white'}}>
-                <Touchable.Circle {...gestureHandler} cx={cx} cy={cy} r={radius} color={'blue'}></Touchable.Circle>
+                <Group>
+                    <Touchable.Circle {...circleGesture} cx={cx} cy={cy} r={radius}></Touchable.Circle>
+                    <Circle cx={windowWidth / 2} cy={windowHeight / 2} r={radius}></Circle>
+                    <SweepGradient c={vec(0, 0)} colors={['cyan', 'magenta', 'cyan']}></SweepGradient>
+                </Group>
             </Touchable.Canvas>
         </GestureHandlerRootView>
   );
