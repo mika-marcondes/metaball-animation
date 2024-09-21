71b2a5eb69cab4d020439dfc6db45bdf72e87a98
Mikael <mika.marcondes2@gmail.com>
2024-09-21 11:58:28 -0300

    Add gesture

--- App.tsx
+++ App.tsx
@@ -1,9 +1,29 @@
-import {Canvas, Circle} from "@shopify/react-native-skia";
+import {useWindowDimensions} from "react-native";
+import Touchable, {useGestureHandler,} from "react-native-skia-gesture"
+import {useSharedValue} from "react-native-reanimated";
+import {GestureHandlerRootView} from "react-native-gesture-handler";
+
+const radius = 80;
 
 export default function App() {
-  return (
-      <Canvas style={{flex: 1, backgroundColor: 'white'}}>
-          <Circle cx={100} cy={100} r={50} color={'blue'}></Circle>
-      </Canvas>
+
+    const {width: windowWidth, height: windowHeight} = useWindowDimensions();
+
+    const cx = useSharedValue(windowWidth / 2);
+    const cy = useSharedValue(windowHeight / 2);
+
+    const gestureHandler = useGestureHandler({
+        onActive: ({translationX, translationY}) => {
+            cx.value = translationX;
+            cy.value = translationY;
+        }
+    })
+
+    return (
+        <GestureHandlerRootView>
+            <Touchable.Canvas style={{flex: 1, backgroundColor: 'white'}}>
+                <Touchable.Circle {...gestureHandler} cx={cx} cy={cy} r={radius} color={'blue'}></Touchable.Circle>
+            </Touchable.Canvas>
+        </GestureHandlerRootView>
   );
 }
