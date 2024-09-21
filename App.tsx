c759ab15e8e4442a65dadb2306380a02abc6974c
Mikael <mika.marcondes2@gmail.com>
2024-09-21 11:22:54 -0300

    Draw circle and fix reanimated

--- App.tsx
+++ App.tsx
@@ -1,20 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {Canvas, Circle} from "@shopify/react-native-skia";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <Canvas style={{flex: 1, backgroundColor: 'white'}}>
+          <Circle cx={100} cy={100} r={50} color={'blue'}></Circle>
+      </Canvas>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
